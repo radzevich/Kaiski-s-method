426d22f8a2fcad6324b8a87af0e37f0b65083310
Alex Radevich <virtuain98@gmail.com>
2016-09-28 15:54:38 +0300

    The fourth commit. Saving results procedures added

--- Kasiski.pas
+++ Kasiski.pas
@@ -4,26 +4,34 @@ interface
 
 implementation
 
-const SUBSTRING_SIZE = 3;
+uses
+  System.SysUtils;
+
+const
+  SUBSTRING_LENGTH = 3;
+  STATISTIC_FILE_NAME = 'statistic.kss';
 
 type
-  PTAnalizeTable = ^TAnalizeTable;
-  TAnalizeTable = record
+  PTStatisticTable = ^TStatisticTable;
+  TStatisticTable = record
     trigram : string[3];
     distance : integer;
-    next : PTAnalizeTable;
+    next : PTStatisticTable;
   end;
 
-function stringsAreEqual(const subString1, subString2 : string[3]) : boolean; forward;
-function greatestCommonDivisor(a, b : integer) : integer; forward; inline;
+var
+  progressiveKeyStepShift : byte;
+
+function stringsAreEqual(var encipheredText : string; const i, j, SUBSTRING_LENGTH : integer) : boolean; forward;
+function greatestCommonDivisor(a, b : integer) : integer; inline; forward;
 function greatestCommonDisvisionExists(const savedDistance, distance : integer) : boolean; forward; inline;
-procedure compareDistances(item : PTAnalizeTable; const  savedDistance, distance : integer); inline; forward;
-procedure addItemToAnalizeTable(var analizeTable : PTAnalizeTable; const trigram : string[3]; 
+procedure compareDistances(item : PTStatisticTable; const  savedDistance, distance : integer); inline; forward;
+procedure addItemToAnalizeTable(var analizeTable : PTStatisticTable; const trigram : string[3];
                                 const numberOfOccurences : integer; const distance : integer); forward;
 
-function createAnalizeTable() : PTAnalizeTable;
+function createAnalizeTable : PTStatisticTable;
 var
-  analizeTable : PTAnalizeTable;
+  analizeTable : PTStatisticTable;
 begin
   new(analizeTable);
   analizeTable^.next := nil;
@@ -31,7 +39,7 @@ begin
   Result := analizeTable;
 end;
 
-procedure addItemToAnalizeTable(const analizeTable : PTAnalizeTable; const trigram : string[3]; 
+procedure addItemToAnalizeTable(const analizeTable : PTStatisticTable; const trigram : string[3];
                                 const distance : integer);
 begin
   new(analizeTable^.next);
@@ -40,8 +48,8 @@ begin
   analizeTable^.next^.next := nil;
 end;
 
-procedure saveDataToAnalizeTable(analizeTable : PTAnalizeTable; const trigram : string[SUBSTRING_SIZE]; 
-                                 const distance : integer);
+procedure saveSubstringToAnalizeTable(analizeTable : PTStatisticTable; const trigram : string[SUBSTRING_LENGTH];
+                                      const distance : integer);
 var
   equal : boolean;
 begin
@@ -68,42 +76,79 @@ begin
   Result := (greatestCommonDivisor(savedDistance, distance) >= 2);
 end;
 
-procedure compareDistances(item : PTAnalizeTable; const  savedDistance, distance : integer); inline;
+procedure compareDistances(item : PTStatisticTable; const  savedDistance, distance : integer); inline;
 begin
   if distance < savedDistance then
     item^.distance := distance;
 end;
 
-procedure FindSubString(const encipheredText : string);
+procedure CompareSubstrings(const encipheredText : string);
 var
   i, j, stringLength : integer;
-  analizeTable : PTAnalizeTable;
+  analizeTable : PTStatisticTable;
 begin
   stringLength := length(encipheredText);
   analizeTable := createAnalizeTable;
 
-  for i := 1 to (stringLength - SUBSTRING_SIZE + 1) do
+  for i := 1 to (stringLength - SUBSTRING_LENGTH + 1) do
     for j := i + 3 to stringLength do
-      if stringsAreEqual(copy(encipheredText, i, SUBSTRING_SIZE), copy(encipheredText, j, SUBSTRING_SIZE))
-      then saveDataToAnalizeTable(analizeTable, copy(encipheredText, i, SUBSTRING_SIZE), j - i);
+      if stringsAreEqual(encipheredText, i, j, SUBSTRING_LENGTH)
+      then saveSubstringToAnalizeTable(analizeTable, copy(encipheredText, i, SUBSTRING_LENGTH), j - i);
 end;
 
-function stringsAreEqual(const subString1, subString2 : string[3]) : boolean;
+function stringsAreEqual(var encipheredText : string; const i, j, SUBSTRING_LENGTH : integer) : boolean;
+var
+  k : integer;
+  flag : boolean;
 begin
-  Result := ((subString1[1] - subString2[1]) = (subString1[2] - subString1[2])) and
-            ((subString1[2] - subString1[2]) = (subString1[3] - subString1[3]));
+  flag := true;
+  k := 0;
+
+  while (k < SUBSTRING_LENGTH - 1) and (flag) do
+  begin
+    flag := (encipheredText[i + k] - encipheredText[j + k]) xor
+            (encipheredText[i + k + 1] - encipheredText[j + k + 1]) = 0;
+    inc(k);
+  end;
+
+  Result := flag;
 end;
 
+procedure saveStatisticToFile(var statisticTable : PTStatisticTable; const trigram : string[3]; ditance : integer);
+var
+  F : file of TStatisticTable;
+  tempItem : TStatisticTable;
+begin
+  assignFile(F, STATISTIC_FILE_NAME);
+
+  if fileExists(STATISTIC_FILE_NAME) then
+  begin
+    Rewrite(F);
+    while statisticTable^.next <> nil do
+    begin
+      tempItem.trigram := statisticTable^.next^.trigram;
+      tempItem.distance := statisticTable^.next^.distance;
+      write(F, tempItem);
+      statisticTable := statisticTable^.next;
+    end;
+    CloseFile(F);
+  end;
+end;
+
+{
 procedure checkSubString(const enchipheredText : string; const staticLeft, scanLeft, subStringSize : integer);
 var
   gcd : integer;
-  target : string;
+  trigram : string;
 begin
   gcd := greatestCommonDivisor(staticLeft, scanLeft);
-  target := Copy(enchipheredText, staticLeft, scanLeft - staticLeft + 1);
-  if gcd >= 3 then SaveResults(target, staticLeft, scanLeft, gcd);
+  trigram := Copy(enchipheredText, staticLeft, scanLeft - staticLeft + 1);
+  if gcd >= 3 then SaveResults(trigram, staticLeft, scanLeft, gcd);
   //здесь мог бы быть ваш частотный анализ;
 end;
+
+procedure saveResults()
+}
 
 function greatestCommonDivisor(a, b : integer) : integer;
 begin
